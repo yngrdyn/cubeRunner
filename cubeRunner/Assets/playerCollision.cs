3b5670923d494c062a2b0d7e8b419b1509926097
Yngrid Coello <yngrdyn@gmail.com>
2017-12-01 20:57:14 +0100

    Adding obstacle to platform

---
+++ cubeRunner/Assets/playerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+	public playerMovement movement;
+
+	void OnCollisionEnter (Collision collisionInfo) {
+		if (collisionInfo.collider.tag == "obstacle") {
+			movement.enabled = false;
+		}
+	}
+}
