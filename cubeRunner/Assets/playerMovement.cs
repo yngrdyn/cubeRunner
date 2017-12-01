194364cc7db18a8ab25c364616a368e7994eb8c7
Yngrid Coello <yngrdyn@gmail.com>
2017-12-01 20:24:53 +0100

    Cube moving forward

---
+++ cubeRunner/Assets/playerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Update applying physics
+	void FixedUpdate () {
+		// Multiplying value with the computer frame 
+		rb.AddForce (0, 0, 2000 * Time.deltaTime);
+	}
+}
